b71009045ff3cb4ecfe7387fd4d0a0e1722dde50
owenstuckman <ostuckman@outlook.com>
2025-04-26 17:21:18 -0400

    temp save before reset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,19 @@
 <script>
-	import '../app.css';
-
-	let { children } = $props();
-</script>
-
-{@render children()}
+	import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+  
+	let { data, children } = $props()
+	let { session, supabase } = $derived(data)
+  
+	onMount(() => {
+	  const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+		if (newSession?.expires_at !== session?.expires_at) {
+		  invalidate('supabase:auth')
+		}
+	  })
+  
+	  return () => data.subscription.unsubscribe()
+	})
+  </script>
+  
+  {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let { data } = $props()
+    let { colors } = $derived(data)
+  </script>
+  
+  <h1>Welcome to Supabase!</h1>
+  <ul>
+    {#each colors as color}
+      <li>{color.name}</li>
+    {/each}
+  </ul>
